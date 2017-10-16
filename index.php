79c065904a3fe70f96a1861fd5dee610517c7f30
Michelle Laforest <lafo0166@algonquinlive.com>
2017-10-16 11:24:21 -0400

    Changed forked files to new ones

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php require_once "includes/variables.php";
+$page = 'Home';
+?>
+
+<?php include "includes/nav.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Strength Through Mindful Movement</title>
+  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="css/styles.css">
+
+  <!--fonts google-->
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,700' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type='text/css'>
+</head>
+<body>
+  <div id="container">
+  <div id="header">
+<h1> Ashleigh Jirik</h1>
+<h2> Strength through mindful movement</h2>
+  </div>
+
+
+<div id=about>
+<h3> about me </h3>
+</div>
+
+<div id=certification>
+<h4> education & certification </h4>
+<h4> past experience </h4>
+</div>
+
+<div id=services>
+<h3> services </h3>
+<p>
+  Improve your play and reduce your risk of injury through this program focused on: Improve your play and reduce your risk of injury through this program focused on:
+</p>
+<br>
+
+  <div class="services1">
+    <img src="images/services.png" alt"" width:100px; height:100px; ">
+    <p>
+      Strength & Conditioning
+    </p>
+  </div>
+  <br>
+  <div class="services2">
+    <img src="images/services.png" alt"" width:100px; height:100px; ">
+    <p>
+      Mobility & Stability
+    </p>
+  </div>
+  <br>
+  <div class="services3">
+    <img src="images/services.png" alt"" width:100px; height:100px; ">
+    <p>
+      Power & Coordination
+    </p>
+  </div>
+
+  <br>
+
+  <div class="services4">
+    <img src="images/services.png" alt"" width:100px; height:100px; ">
+    <p>
+      Enhanced Aerobic Capacity
+     </p>
+  </div>
+
+  <br>
+
+  <div class="services5">
+    <img src="images/services.png" alt"" "width:100px; height:100px; ">
+    <p>
+       Nutritional Education
+    </p>
+  </div>
+</div>
+  <br>
+
+  <div id=Testimonials>
+  <h4> testimonials </h4>
+  <img src="images/testimonials.png" alt"" "width:50px; height:50px; ">
+
+    <p>
+      "To turn a lifelong couch potato into a gym rat is no small feat. Teresa is terrific. She suggests exercise routines that are individualized and challenging, but not more than you can handle. Also, she is a very interesting -- and interested -- person."
+      <br> <br> A. R., gym rat, Ottawa, ON
+      </p>
+  </div>
+
+  <br>
+  <div id=map>
+  <h4> where we'll work </h4>
+
+  </div>
+  <div id=contact>
+  <h4> get in touch </h4>
+
+  </div>
+
+</div>
+
+
+
+</div>
+</body>
+</html>
